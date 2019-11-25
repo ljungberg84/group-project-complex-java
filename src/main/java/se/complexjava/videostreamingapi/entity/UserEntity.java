ab74f8542bb51618db2e4a9190e094fee144cc25
Carl Ljungberg <ljungberg125@gmail.com>
2019-11-25 16:30:05 +0100

    add spring-boot-starter-jpa dependency, add UserEntity

---
+++ src/main/java/se/complexjava/videostreamingapi/entity/UserEntity.java
@@ -0,0 +1,10 @@
+package se.complexjava.videostreamingapi.entity;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Entity
+public class UserEntity implements Serializable {
+
+
+}
