f29301a0f459f850d03e5c428abaa320f4e48f35
Gustav Ljungberg <g.ljungberg@live.se>
2019-12-10 15:37:45 +0100

    composite key for flag entity

---
+++ src/main/java/se/complexjava/videostreamingapi/entity/composite_key/FlagKey.java
@@ -0,0 +1,24 @@
+package se.complexjava.videostreamingapi.entity.composite_key;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class FlagKey implements Serializable{
+
+	Long userId;
+	
+	Long videoId;
+}
