1e0df32e1730d5b84fa2c2a6e250498a72208e26
Gustav Ljungberg <g.ljungberg@live.se>
2019-12-11 18:10:40 +0100

    FlagVideoModel

---
+++ src/main/java/se/complexjava/videostreamingapi/model/FlagVideoModel.java
@@ -0,0 +1,45 @@
+package se.complexjava.videostreamingapi.model;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import se.complexjava.videostreamingapi.entity.FlagVideo;
+import se.complexjava.videostreamingapi.entity.User;
+import se.complexjava.videostreamingapi.entity.Video;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FlagVideoModel {
+
+	private long id;
+	User user;
+	Video video;
+	Instant dateFlagged;
+	
+	
+	private static ModelMapper modelMapper = new ModelMapper();
+	
+	public static FlagVideoModel fromEntity(FlagVideo entity) {
+		return modelMapper.map(entity, FlagVideoModel.class);
+	}
+	
+	public static List<FlagVideoModel> fromEntity(Iterable<FlagVideo> entities) {
+		
+		List<FlagVideoModel> models = new ArrayList<>();
+		for(FlagVideo entity : entities) {
+			models.add(fromEntity(entity));
+		}
+		
+		return models;
+	}
+	
+}
