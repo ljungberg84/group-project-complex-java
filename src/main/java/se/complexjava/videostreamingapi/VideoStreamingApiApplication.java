f48377d92711698468c306dc5eeef328caeedb70
Maksym-Gorbunov <maxsverige@hotmail.com>
2020-01-22 15:12:02 +0100

    @EnableJms, Fabric8 in POM file

--- src/main/java/se/complexjava/videostreamingapi/VideoStreamingApiApplication.java
+++ src/main/java/se/complexjava/videostreamingapi/VideoStreamingApiApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableJms
 @EnableEurekaClient
 @SpringBootApplication
 public class VideoStreamingApiApplication {
